b7c0a456681758978f67cc179c6411924dddb28c
evanpservito <evanpservito@gmail.com>
2023-09-06 17:09:13 -0700

    create custom message page, hide links in navbar during logout

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
-import PendingPackages from "./pages/PackageStatus/PackageStatus";
+import PackageStatus from "./pages/PackageStatus/PackageStatus";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 import { AuthContextProvider } from "./context/AuthContext";
 import Protected from "./components/Protected";
 import { Navbar } from "./components/Navbar/Navbar";
+import CustomMessage from "./pages/CustomMessage/CustomMessage";
 
 function App() {
   return (
@@ -28,10 +29,18 @@ function App() {
               }
             />
             <Route
-              path="/pending-packages"
+              path="/package-status"
               element={
                 <Protected>
-                  <PendingPackages />
+                  <PackageStatus />
+                </Protected>
+              }
+            />
+            <Route
+              path="/custom-message"
+              element={
+                <Protected>
+                  <CustomMessage />
                 </Protected>
               }
             />
