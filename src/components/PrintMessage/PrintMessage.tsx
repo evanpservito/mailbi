d63a85ea4e42523b901c17bd58f66a4e418dbde4
evanpservito <evanpservito@gmail.com>
2023-12-03 19:18:01 -0800

    add signatures section on print message

--- src/components/PrintMessage/PrintMessage.tsx
+++ src/components/PrintMessage/PrintMessage.tsx
@@ -12,18 +12,24 @@ const PrintMessage = ({ mailboxNumber, trackingNumber, packageType }: any) => {
         <Text as="b" fontSize="4xl">
           Mailpost Alert! You have a package ready for pickup.
         </Text>
-        <Text fontSize="2xl">Mailbox Number: {mailboxNumber} </Text>
-        <Text fontSize="2xl">Tracking Number: {trackingNumber}</Text>
-        <Text fontSize="2xl">Package Type: {packageType}</Text>
-        <Text fontSize="2xl">Delivery Date: {currentDate}</Text>
+        <Text fontSize="xl">Mailbox Number: {mailboxNumber} </Text>
+        <Text fontSize="xl">Tracking Number: {trackingNumber}</Text>
+        <Text fontSize="xl">Package Type: {packageType}</Text>
+        <Text fontSize="xl">Delivery Date: {currentDate}</Text>
+
+        <div className="signatures">
+          <Text>Staff Signature: ____________________</Text>
+          <Text>Customer Signature: ____________________</Text>
+        </div>
       </div>
       <div className="print-sheet-second">
         <Text fontSize="2xl">Mailbox Number:</Text>
         <Text as="b" fontSize="8xl">
           {mailboxNumber}
         </Text>
-        <Text fontSize="3xl">Package Type: {packageType}</Text>
-        <Text fontSize="3xl">Delivery Date: {currentDate}</Text>
+        <Text fontSize="xl">Tracking Number: {trackingNumber}</Text>
+        <Text fontSize="xl">Package Type: {packageType}</Text>
+        <Text fontSize="xl">Delivery Date: {currentDate}</Text>
       </div>
     </div>
   );
