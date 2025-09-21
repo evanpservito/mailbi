bd786e14790bad94881c2e6d6a90c5052d3c3174
Evan Servito <78441979+evanpservito@users.noreply.github.com>
2025-09-20 21:03:10 -0700

    initialize shadcn

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,57 +1,41 @@
 "use client";
 
-import { AtSign, Key, CircleAlert, MoveRight } from "lucide-react";
+import { CircleAlert, MoveRight } from "lucide-react";
 
 import { useFormStatus } from "react-dom";
 import { useActionState } from "react";
 import { handleSignIn } from "@/lib/cognitoActions";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Login() {
   const [errorMessage, dispatch] = useActionState(handleSignIn, undefined);
   return (
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={` mb-3 text-2xl`}>Please log in to continue.</h1>
+        <h1 className={`mb-3 text-2xl`}>Login</h1>
         <div className="w-full">
           <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-              <AtSign className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
+            <Label htmlFor="email">Email</Label>
+            <Input
+              className=""
+              type="email"
+              id="email"
+              placeholder="Email Address"
+              required
+            />
           </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-              <Key className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
+          <div>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              className=""
+              id="password"
+              type="password"
+              placeholder="Enter password"
+              required
+              minLength={6}
+            />
           </div>
         </div>
         <LoginButton />
